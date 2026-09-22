f9c42a5c5a64de905a4a0e967f356ee523dcf677
Matheus <lemos.matheus@escolar.ifrn.edu.br>
2026-09-22 16:39:30 -0300

    feat: adição completa do seguir linha e movimento

--- src/Estrategia.cpp
+++ src/Estrategia.cpp
@@ -9,21 +9,35 @@ Estrategia::~Estrategia()
 }
 
 void Estrategia::SeguirLinha() {
-    sensorLinha.lerSensor();
-    if (sensorLinha.branco()){
-        Serial.println("ta tudo branco");
-    }else if (sensorLinha.preto()) {
-        Serial.println("ta tudo preto");
-    } else {
-        Serial.println("deu erro");
+    if (sensorLinha.ext_branco() || sensorLinha.branco()) {
+        movimento.frente();
+    } else if (sensorLinha.Esq_preto()) {
+        movimento.esquerda();
+    } else if (sensorLinha.Dir_preto()) {
+        movimento.direita();
+    } else if (sensorLinha.ladoEsq_preto()) {
+        movimento.esquerda_medio();
+    } else if (sensorLinha.ladoDir_preto()) {
+        movimento.direita_medio();
+    } else if (sensorLinha.maisEsq_preto()) {
+        movimento.esquerda_forte();
+    } else if (sensorLinha.maisDir_preto()) {
+        movimento.direita_forte();
+    } else if (sensorLinha.maisEsq_branco()) {
+        movimento.direita_forte();
+    } else if (sensorLinha.maisDir_branco()) {
+        movimento.esquerda_forte();
+    } else if (sensorLinha.preto()) {
+        // lembrar de implementar a estrategia da encruzilhada
+        movimento.frente();
     }
 }
-
 void Estrategia::iniciar() {
     Serial.begin(9600);
     robo.configurar();
 }
 
 void Estrategia::executar() {
+    // implementar condição para executar estrategias diferentes em determinadas condiçõess
     SeguirLinha();
 }
